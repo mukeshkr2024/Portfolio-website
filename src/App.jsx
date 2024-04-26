fe832c29f25daf7cc3bc2a6800f236a5e9649fd5
mukesh kumar <mkmehta2041@gmail.com>
2024-04-26 12:03:48 +0530

    added fonts

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import prateekImg from "./assets/prateek-prasoon.png";
 import company1 from "./assets/company-1.svg";
 import company2 from "./assets/company-2.svg";
+import facebookLogo from "./assets/facebook-logo.svg";
 
 const posts = [
   {
@@ -40,51 +41,49 @@ const App = () => {
           </span>
         </div>
         <div className="flex w-full">
-          <div className="w-[40%] flex flex-col gap-y-6">
-            <h4 className="text-[53px] font-semibold">
-              I help fill your business gaps{" "}
-            </h4>
+          <div className="flex-1">
+            <div className="max-w-lg flex flex-col gap-y-6">
+              <h4 className="text-[53px] font-semibold font-outFit lg:leading-[71.4px]">
+                I help fill your business gaps{" "}
+              </h4>
 
-            <h5 className="text-[38px] font-medium text-[#5D5D5D]">
-              Saving your day, Every day.
-            </h5>
+              <h5 className="text-[38px] font-medium text-[#5D5D5D] font-outFit">
+                Saving your day, Every day.
+              </h5>
 
-            <p className="text-[#000000] text-lg">
-              I understand your firm&apos;s fundamental needs: to either
-              increase revenue, save costs, or save time. I don&apos;t just talk
-              about expertise; I deliver it.
-            </p>
+              <p className="text-lg font-openSans text-[#4E4E4E]">
+                I understand your firm&apos;s fundamental needs: to either
+                increase revenue, save costs, or save time. I don&apos;t just
+                talk about expertise; I deliver it.
+              </p>
 
-            <div className="flex w-full items-start gap-6">
-              <button
-                className="w-[180px] h-[55px] bg-[#181818] rounded-[50px] text-white font-bold text-xl"
-                // style={{
-                //   boxShadow: "#fff 2px 2px 0 0, #000 2px 2px 0 1.5px",
-                //   transform: "translate(2px, 2px)",
-                // }}
-                style={{
-                  boxShadow: "#fff 4px 5px 0 0, #000 4px 5px 0 1.5px",
-                  transform: "translate(2px, 2px)",
-                }}
-              >
-                Book a call
-              </button>
-              <button
-                className="w-[180px] h-[55px] border-2 border-[#04080A] text-xl font-bold rounded-[50px]"
-                style={{
-                  boxShadow: "#fff 4px 5px 0 0, #000 4px 5px 0 1.5px",
-                  transform: "translate(2px, 2px)",
-                }}
-              >
-                Our Courses
-              </button>
+              <div className="flex w-full items-start gap-6 font-openSans">
+                <button
+                  className="w-[180px] h-[55px] bg-[#181818] rounded-[50px] text-white font-bold text-xl"
+                  style={{
+                    boxShadow: "#fff 4px 5px 0 0, #000 4px 5px 0 1.5px",
+                    transform: "translate(2px, 2px)",
+                  }}
+                >
+                  Book a call
+                </button>
+                <button
+                  className="w-[180px] h-[55px] border-2 border-[#04080A] text-xl font-bold rounded-[50px]"
+                  style={{
+                    boxShadow: "#fff 4px 5px 0 0, #000 4px 5px 0 1.5px",
+                    transform: "translate(2px, 2px)",
+                  }}
+                >
+                  Our Courses
+                </button>
+              </div>
             </div>
           </div>
-          <div className="w-[60%] flex items-center justify-center">
+          <div className="flex-1 flex items-center justify-center  ">
             <img
               src={prateekImg}
               alt="Prateek Prasoon"
-              className="w-[90%] -mt-14"
+              className="w-full -mt-14 -mr-16"
             />
           </div>
         </div>
@@ -93,10 +92,10 @@ const App = () => {
       <section className="bg-[#F3F3F1] w-full px-20 py-10 ">
         <div className="w-full flex">
           <div className="flex-1">
-            <h5 className="text-[#000000] text-[48px] font-medium">
+            <h5 className="text-[#000000] text-[48px] font-medium font-outFit">
               Prateek Prasoon
             </h5>
-            <div className="text-[19px] font-normal mt-10 flex flex-col gap-y-6">
+            <div className="text-[19px] font-normal mt-10 flex flex-col gap-y-6 text-[#4E4E4E] font-openSans">
               <p>
                 CloudPrism Solutions isn&apos;t just a workplace—it&apos;s a
                 community. Here, you&apos;re not just an employee; you&apos;re a
@@ -129,10 +128,10 @@ const App = () => {
       <section className="px-20 py-10">
         <div className="flex w-full">
           <div className="flex-1">
-            <h5 className="text-[50px] font-semibold">
+            <h5 className="text-[50px] font-semibold font-outFit">
               How CloudPrism can help you?
             </h5>
-            <div className="mt-10 text-[20px] text-[#4E4E4E] flex flex-col gap-y-6">
+            <div className="mt-10 text-[20px]  flex flex-col gap-y-6 text-[#4E4E4E] font-openSans">
               <p>
                 CloudPrism Solutions isn&apos;t just a workplace—it&apos;s a
                 community. Here, you&apos;re not just an employee; you&apos;re a
@@ -152,10 +151,10 @@ const App = () => {
 
         <div className="flex w-full mt-10">
           <div className="flex-1">
-            <h5 className="text-[50px] font-semibold">
+            <h5 className="text-[50px] font-semibold font-outFit">
               How Coding Commando can help you?{" "}
             </h5>
-            <div className="mt-10 text-[20px] text-[#4E4E4E] flex flex-col gap-y-6">
+            <div className="mt-10 text-[20px] text-[#4E4E4E] flex flex-col gap-y-6 font-openSans">
               <p>
                 CloudPrism Solutions isn&apos;t just a workplace—it&apos;s a
                 community. Here, you&apos;re not just an employee; you&apos;re a
@@ -179,7 +178,7 @@ const App = () => {
       </section>
 
       <section className="mt-10">
-        <h4 className="text-center text-[43px] font-semibold">
+        <h4 className="text-center text-[43px] font-semibold font-outFit">
           Reach out to Prateek Prasoon
         </h4>
 
@@ -233,7 +232,22 @@ const App = () => {
         </form>
       </section>
 
-      <section className="bg-[#2E2E2E] h-52"></section>
+      <section className="bg-[#2E2E2E] h-28 px-20 flex items-center">
+        <div className="flex gap-2">
+          <a className="size-9 bg-white rounded-full flex items-center justify-center">
+            <img src={facebookLogo} alt="" className="size-8" />
+          </a>
+          <a className="size-9 bg-white rounded-full flex items-center justify-center">
+            <img src={facebookLogo} alt="" className="size-8" />
+          </a>
+          <a className="size-9 bg-white rounded-full flex items-center justify-center">
+            <img src={facebookLogo} alt="" className="size-8" />
+          </a>
+          <a className="size-9 bg-white rounded-full flex items-center justify-center">
+            <img src={facebookLogo} alt="" className="size-8" />
+          </a>
+        </div>
+      </section>
     </div>
   );
 };
@@ -243,9 +257,13 @@ function PostItem({ title, description }) {
     <div className="w-[270px]">
       <div className="w-full h-[175px] bg-[#D9D9D9] rounded-[14px]"></div>
       <div className="flex flex-col items-start gap-y-2 mt-4">
-        <h3 className="text-lg text-[#181818] font-bold">{title}</h3>
-        <p className="text-[15px] text-[#4E4E4E]">{description}</p>
-        <button className="bg-[#181818] rounded-[57px] text-white text-[17px] font-medium px-8 py-1.5">
+        <h3 className="text-lg text-[#181818] font-bold font-openSans">
+          {title}
+        </h3>
+        <p className="text-[15px] text-[#4E4E4E] font-openSans">
+          {description}
+        </p>
+        <button className="bg-[#181818] rounded-[57px] text-white text-[17px] font-medium px-8 py-1.5 font-outFit">
           Read
         </button>
       </div>
@@ -255,7 +273,7 @@ function PostItem({ title, description }) {
 
 function InputBtn({ label }) {
   return (
-    <div className="bg-[#181818] text-white rounded-[57px] w-[120px] h-[42px] font-medium flex items-center justify-center">
+    <div className="bg-[#181818] text-white rounded-[57px] w-[120px] h-[42px] font-medium flex items-center justify-center font-outFit">
       {label}
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    fontFamily: {
+      openSans: ["Open Sans", "sans-serif"],
+      outFit: ["Outfit", "sans-serif"],
+    },
   },
   plugins: [],
 };
